7a867d00bf32c1627ed777a4be1bbc6426305504
Lukas Köllner <lkoe@bluewin.ch>
2024-04-15 08:40:34 +0000

    Mute functionality for all the sounds

--- app/javascript/controllers/pause_controller.js
+++ app/javascript/controllers/pause_controller.js
@@ -1,7 +1,25 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import $ from "jquery";
 
 // Connects to data-controller="pause"
 export default class extends Controller {
-  connect() {
-  }
+    connect() {
+        this.paused = false;
+        this.audio = $(
+            '*[data-controller="simple-sound"], *[data-controller="sound"]',
+        );
+        console.log(this.simple_audio);
+    }
+
+    toggle() {
+        this.paused = !this.paused;
+        if (this.paused) {
+            this.audio.data("muted", true);
+            console.log(this.audio);
+            $(this.element).text("Re-enable sounds");
+        } else {
+            this.audio.removeData("muted");
+            $(this.element).text("Disable all sounds");
+        }
+    }
 }
---
+++ app/javascript/controllers/simple_sound_controller.js
@@ -0,0 +1,81 @@
+import { Controller } from "@hotwired/stimulus";
+import $ from "jquery";
+
+// Connects to data-controller="simple-sound"
+export default class extends Controller {
+    static targets = [
+        "audio",
+        "background",
+        "play",
+        "pause",
+        "probability",
+        "probabilityDisplay",
+        "name",
+    ];
+
+    connect() {
+        this.interval = 1000;
+        this.enabled = false;
+        this.played = 0;
+        this.time = 0;
+        setInterval(this.interval_fn.bind(this), this.interval);
+
+        let background_url = $(this.backgroundTarget).attr("src");
+        this.element.style.backgroundImage = `url("${background_url}")`;
+        this.update_probability_display();
+        $(this.probabilityTarget).on(
+            "input",
+            this.update_probability_display.bind(this),
+        );
+    }
+
+    play() {
+        $(this.playTarget).addClass("hidden");
+        $(this.pauseTarget).removeClass("hidden");
+        this.enabled = true;
+    }
+
+    pause() {
+        $(this.playTarget).removeClass("hidden");
+        $(this.pauseTarget).addClass("hidden");
+        this.enabled = false;
+        this.audioTarget.pause();
+        this.audioTarget.currentTime = 0;
+    }
+
+    interval_fn() {
+        let probability = $(this.probabilityTarget).val();
+        if (!this.enabled) return;
+        if (!this.audioTarget.paused) return;
+        if ($(this.element).data("muted")) return;
+        if (Math.random() > this.probability_calc(probability) / 100)
+            return this.time++;
+        this.audioTarget.play();
+        this.played++;
+        let average = this.time / this.played;
+        let unit = average > 60 ? "min" : "s";
+        if (average > 60) average /= 60;
+        average = Math.round(average);
+        console.log(
+            `Average interval for ${this.nameTarget.innerText}: ${average}${unit}`,
+        );
+    }
+
+    probability_calc(x) {
+        return 4 / (1 + Math.exp(-(x / 10.3) + 4.7));
+    }
+
+    update_probability_display() {
+        let disp = $(this.probabilityDisplayTarget);
+        let x = $(this.probabilityTarget).val();
+        let probability = this.probability_calc(x);
+        let unit = "s";
+        let interval = ((100 / probability) * this.interval) / 1000;
+        if (interval > 60) {
+            interval /= 60;
+            unit = "min";
+        }
+        interval = Math.round(interval);
+        disp.text(`~${interval}${unit}`);
+    }
+}
